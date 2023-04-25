ccb32a22d7cf9512caadafcfd62fa1c7500d545c
kalu_coder <awesomecoding51@gmail.com>
2023-04-25 23:22:36 +0300

    comments added

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+'''
+A python script that contians a flask app
+'''
 
 from flask import Flask, render_template
 
@@ -6,6 +9,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    The root page
+    """
     return render_template('0-index.html')
 
 if __name__ == "__main__":

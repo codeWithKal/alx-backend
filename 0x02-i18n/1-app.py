7ea9fcd9659a58e2195a665440d6ac6fe4e46f54
kalu_coder <awesomecoding51@gmail.com>
2023-04-26 13:40:54 +0300

    index template returner added and app made run

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -18,3 +18,14 @@ class Config:
 app.config.from_object(Config)
 
 babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """index routing method
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
